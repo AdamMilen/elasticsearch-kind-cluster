fc62e99e1bd856716539fecfc791e506dd2d0edd
Adam Milenovich <adamgmilenovich5@gmail.com>
2025-02-09 18:25:01 +0200

    adding linkerd installation phase

--- initial_setup.sh
+++ initial_setup.sh
@@ -10,6 +10,8 @@ kubectl wait pods --all -n argocd --for condition=Ready --timeout=90s
 # login in to argocd instance and adding repo
 PASSWORD="$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
 kubectl port-forward -n argocd service/argocd-server 8443:443 &
+echo "login into argocd and adding repo"
+sleep 8
 argocd login localhost:8443 --username admin --password "$PASSWORD" --insecure
 argocd repo add https://github.com/AdamMilen/elasticsearch-assignment.git --username AdamMilen --password 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
 
@@ -19,7 +21,14 @@ read -p "Do you want to continue and install linkerd? y/n " answer
 if [ "$answer" == "y" ]; then
 	linkerd install --crds | kubectl apply -f -
 	linkerd install | kubectl apply -f -
-	echo $'Script finished\nYour username and password for argocd ui: admin\n${PASSWORD}'
+	echo "waiting for linkerd control plane to be ready"
+	kubectl wait pods --all -n linkerd --for condition=Ready --timeout=120s
+	linkerd viz install | kubectl apply -f - # install the on-cluster metrics stack
+	echo "waiting for linkerd extension to be ready"
+	kubectl wait pods --all -n linkerd-viz --for condition=Ready --timeout=120s
+	linkerd check
+	linkerd viz dashboard &
+	echo -e "Script finished\nYour username and password for argocd ui:\nadmin\n${PASSWORD}"
 else
 	exit
 fi
